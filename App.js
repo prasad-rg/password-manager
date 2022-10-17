404aee901b2c9a5d7837d3f67039b1a51e2f038b
Prasad R G <prasadrg@PrasadRG-iMac.local>
2022-10-17 16:25:54 +0530

    Created List View Component

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {StyleSheet, View, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import ListView from './src/components/ListView';
 import AuthTabNavigator from './src/navigation/AuthTabNavigator';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      {/* <AuthScreenHolder /> */}
-      <AuthTabNavigator />
-      {/* <SignUpScreen /> */}
+      {/* <AuthTabNavigator /> */}
+      <ListView />
     </View>
   );
 };
@@ -15,6 +15,9 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FAFAFA',
   },
 });
 export default App;
---
+++ src/components/ListView.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {View, Text, Pressable, StyleSheet, Image} from 'react-native';
+import imgUrl from '../../assets/images/Bitmap.png';
+
+const ListView = ({title = 'Facebook', url = 'www.facebook.com', uri}) => {
+  return (
+    <Pressable style={styles.listContainer}>
+      <View style={styles.rowView}>
+        <View>
+          <Image source={uri ? uri : imgUrl} />
+        </View>
+        <View>
+          <Text style={styles.siteText}>Facebook</Text>
+          <Pressable>
+            <Text style={styles.copyPasswordText}>Copy Password</Text>
+          </Pressable>
+        </View>
+      </View>
+      <View style={styles.urlContainer}>
+        <Text style={styles.urlText}>www.facebook.com</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  listContainer: {
+    height: 104,
+    width: 320,
+    borderRadius: 12,
+    backgroundColor: '#FFFFFF',
+    elevation: 10,
+    shadowColor: 'rgba(0,0,0,0.02)',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  rowView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 18,
+    paddingTop: 10,
+    paddingEnd: 10,
+    width: 320,
+    height: 68,
+  },
+  urlContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FAFAFA',
+    borderBottomEndRadius: 12,
+    borderBottomStartRadius: 12,
+    overflow: 'hidden',
+    fontSize: 14,
+    height: 36,
+    width: '100%',
+  },
+  siteText: {
+    fontFamily: 'OpenSans-Semibold',
+    fontSize: 18,
+    color: '#0E85FF',
+  },
+  copyPasswordText: {
+    // fontFamily: 'OpenSans-Regular',
+    color: '#0E85FF',
+    fontSize: 13,
+  },
+  urlText: {
+    // fontFamily: 'OpenSans-Regular',
+    color: '#3C4857',
+  },
+});
+
+export default ListView;
